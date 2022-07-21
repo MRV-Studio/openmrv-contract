d9f940b4d7cf7ef0136621035b048c61f07cbaa5
tsondru <k.tsondru.tarchin@pm.me>
2022-07-21 09:20:09 -0600

    use bytes for hash

--- contracts/GeodataAnchor.sol
+++ contracts/GeodataAnchor.sol
@@ -4,16 +4,16 @@ pragma solidity ^0.8.13;
 contract GeodataAnchor {
 
     struct Anchor {
-        bytes32 hash;
+        bytes hash;
     }
 
     mapping(string => Anchor) private anchors; // mapping of id to anchor
 
     // add anchor
-    function addAnchor(string memory _id, bytes32 _hash) public {
+    function addAnchor(string memory _id, bytes memory _hash) public {
         // see if anchor already exists
         Anchor storage anchor = anchors[_id];
-        if (anchor.hash != 0) {
+        if (anchor.hash.length > 0) {
             revert("Anchor id already exists");
         }
 
